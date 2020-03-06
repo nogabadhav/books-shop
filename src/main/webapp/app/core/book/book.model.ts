dd0c14cf30ffc8a95291473618853f41af0f8a9b
nogabadhav <nogabad@walla.com>
2020-03-06 22:37:57 +0200

    recommended + images

--- src/main/webapp/app/core/book/book.model.ts
+++ src/main/webapp/app/core/book/book.model.ts
@@ -2,8 +2,9 @@ export interface IBook {
   id?: any;
   name?: string;
   imageUrl?: string;
+  price?: number;
 }
 
 export class Book implements IBook {
-  constructor(public id?: any, public name?: string, public imageUrl?: string) {}
+  constructor(public id?: any, public name?: string, public imageUrl?: string, price?: number) {}
 }
